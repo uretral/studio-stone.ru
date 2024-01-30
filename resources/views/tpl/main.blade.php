ce7f9ba67ce654eeefa65d0cdb25bd2ad6b0e57f
murindev <cdnadom@gmail.com>
2024-01-30 13:20:18 +0300

    1

--- resources/views/tpl/main.blade.php
+++ resources/views/tpl/main.blade.php
@@ -7,11 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/css/app.css','resources/js/app.js'])
 
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ vite::asset('resources/img/favicon/apple-touch-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ vite::asset('resources/img/favicon/favicon-32x32.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ vite::asset('resources/img/favicon/favicon-16x16.png')}}">
-    <link rel="manifest" href="{{ vite::asset('resources/img/favicon/site.webmanifest')}}">
-    <link rel="mask-icon" href="{{ vite::asset('resources/img/favicon/safari-pinned-tab.svg')}}" color="#5bbad5">
+{{--    <link rel="apple-touch-icon" sizes="180x180" href="{{ vite::asset('resources/img/favicon/apple-touch-icon.png')}}">--}}
+{{--    <link rel="icon" type="image/png" sizes="32x32" href="{{ vite::asset('resources/img/favicon/favicon-32x32.png')}}">--}}
+{{--    <link rel="icon" type="image/png" sizes="16x16" href="{{ vite::asset('resources/img/favicon/favicon-16x16.png')}}">--}}
+{{--    <link rel="manifest" href="{{ vite::asset('resources/img/favicon/site.webmanifest')}}">--}}
+{{--    <link rel="mask-icon" href="{{ vite::asset('resources/img/favicon/safari-pinned-tab.svg')}}" color="#5bbad5">--}}
+
+    <link rel="icon" href="favicon.ico">
 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="robots" content="index, follow">
