eb318e094773faadeb8452146cb73fb2741cc35e
murindev <cdnadom@gmail.com>
2024-01-30 13:25:24 +0300

    1

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,98 @@
+<footer class="motopress-wrapper footer">
+    <div class="container">
+        <div class="row">
+            <div class="span12" data-motopress-wrapper-file="wrapper/wrapper-footer.php" data-motopress-wrapper-type="footer" data-motopress-id="5a52228b539a9">
+                <div class="row footer-widgets">
+                    <div class="span12" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-1">
+                        <div id="social_networks-2">
+                            <!-- BEGIN SOCIAL NETWORKS -->
+
+                            <ul class="social social__row clearfix unstyled">
+
+
+                            </ul>
+                            <!-- END SOCIAL NETWORKS -->
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row copyright">
+                    <div style="display: table; width: 100%;">
+                        <div class="span" style="display:table-cell; height: 100%; vertical-align: middle;">
+								<span class="footer-text" style="vertical-align: inherit">
+									Наши партнеры <a target="_blank" href="http://www.internal.su/">
+										<img src="/local/assets/images/logo-ex.png" alt="logo" width="25%" style="vertical-align: middle; padding-left: 15px;">
+									</a>
+
+                                    <!-- {%FOOTER_LINK} -->
+								</span>
+                        </div>
+                        <div class="span6" style="display:table-cell; height: 100%; vertical-align: middle;">
+
+								<span class="footer-text" style="vertical-align: inherit">
+									<!-- Yandex.Metrika informer --> <a href="https://metrika.yandex.ru/stat/?id=48141974&amp;from=informer" target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/48141974/3_0_FFFFFFFF_EFEFEFFF_0_pageviews" style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="48141974" data-lang="ru"></a> <!-- /Yandex.Metrika informer --> <!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter48141974 = new Ya.Metrika2({ id:48141974, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/tag.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks2"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/48141974" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+								</span>
+
+
+
+                            <!-- {%FOOTER_LINK} -->
+
+                        </div>
+                        <div class="span6" style="display:table-cell; height: 100%; vertical-align: middle;">
+
+								<span class="footer-text edit" style="vertical-align: inherit">
+									        <form class="admin" id="text_7" method="post">
+        <input type="hidden" name="ID" value="7">
+        <textarea name="UF_SOURCE">								</textarea>
+
+        <fieldset>
+            <button>Обновить</button>
+            <button type="button" class="close-form">Закрыть</button>
+        </fieldset>
+
+    </form>
+
+																</span>
+
+
+
+                            <!-- {%FOOTER_LINK} -->
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+<!--FOOTER SCRIPTS-->
+<div id="back-top-wrapper" class="visible-desktop">
+    <p id="back-top">
+        <a href="#top"><span></span></a>
+    </p>
+</div>
+<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter48141974 = new Ya.Metrika2({ id:48141974, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/tag.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks2"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/48141974" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+
+
+<script type='text/javascript'>
+    /* <![CDATA[ */
+    var _wpcf7 = {"loaderUrl":"https:\/\/livedemo00.template-help.com\/wordpress_48469\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif","sending":"Sending ..."};
+    /* ]]> */
+</script>
+
+
+    <!--LiveInternet counter--><script type="text/javascript">
+    document.write('<a href="//www.liveinternet.ru/click" '+
+        'target="_blank"><img src="//counter.yadro.ru/hit?t50.6;r'+
+        escape(document.referrer)+((typeof(screen)=='undefined')?'':
+            ';s'+screen.width+'*'+screen.height+'*'+(screen.colorDepth?
+                screen.colorDepth:screen.pixelDepth))+';u'+escape(document.URL)+
+        ';h'+escape(document.title.substring(0,150))+';'+Math.random()+
+        '" alt="" title="LiveInternet" '+
+        'border="0" width="31" height="31"><\/a>')
+</script><!--/LiveInternet-->
+
+
+
