f3443011a2964be9a1531ebadb3bc493c25ed399
murindev <cdnadom@gmail.com>
2024-01-31 05:17:18 +0300

    1

---
+++ app/Models/TopSlider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TopSlider extends Model
+{
+    use HasFactory;
+}
